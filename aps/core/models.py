02061f6bd40df5bb1a548d23b06a7577146b329a
Arjun Mittu <43925857+arjun-mittu@users.noreply.github.com>
2020-06-28 23:54:20 +0530

    add to cart

--- aps/core/models.py
+++ aps/core/models.py
@@ -36,3 +36,29 @@ class Item(models.Model):
         return reverse("core:products", kwargs={
             'slug': self.slug
         })
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.title}"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
--- aps/core/urls.py
+++ aps/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import checkout,HomeView,ItemDetailView
+from .views import checkout,HomeView,ItemDetailView,add_to_cart
 
 app_name = 'core'
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
     path('checkout/',checkout,name='checkout'),
-    path('product/<slug>/',ItemDetailView.as_view(),name='products'),
+    path('product/<slug>/',ItemDetailView.as_view(),name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
 ]
