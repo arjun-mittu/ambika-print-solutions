070e919960c2a8dc13955036a4c92eefa813b896
Arjun Mittu <43925857+arjun-mittu@users.noreply.github.com>
2020-06-29 03:01:35 +0530

    cart

--- aps/core/models.py
+++ aps/core/models.py
@@ -40,6 +40,10 @@ class Item(models.Model):
         return reverse("core:add-to-cart", kwargs={
             'slug': self.slug
         })
+    def get_remove_from_cart_url(self):
+        return reverse("core:remove-from-cart", kwargs={
+            'slug': self.slug
+        })
 
 class OrderItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -50,8 +54,16 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.item.title}"
-
-
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+    def get_total_discount_item_price(self):
+        return self.quantity * self.item.discount_price
+    def get_amount_saved(self):
+        return self.get_total_item_price() - self.get_total_discount_item_price()
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_total_discount_item_price()
+        return self.get_total_item_price()
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
@@ -62,3 +74,8 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        return total
